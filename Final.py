771f93ab5a9442fab32ec22e49dd76648de842c8
Akshit35 <96778124+Akshit35@users.noreply.github.com>
2022-01-14 16:15:29 +0530

    Assignment1
    Akshit Singhal(21104080)

---
+++ Final.py
@@ -0,0 +1,58 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:/Users/hp/OneDrive/Desktop/Akshit (21104080).py ==========
+Average Calculator
+Enter the first number:2
+Enter the second number:3
+Enter the third number:4
+Average of three numbers is: 3.0
+
+
+
+
+Income Tax Calculator
+Enter your gross income:20000
+Enter the number of dependents:2
+Your total income tax: 800.0
+
+
+
+
+Student Details list
+Enter your Name:Rohan
+Enter your Student id:10
+Gender(F,M,U):M
+CGPA:9.1
+Course Name:EE
+('Rohan', [10, 'Rohan', 'M', 'EE', 9.1])
+
+
+
+
+Student Mark List
+Marks of first student:2
+Marks of second student:3
+Marks of third student:9
+Marks of fourth student:5
+Marks of fifth student:4
+['2', '3', '4', '5', '9']
+
+
+
+
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+
+
+
+After deleting fourth item,new list is: ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+
+
+
+Final List: ['Red', 'Green', 'White', 'Purple', 'Yellow']
+
+
+
+
