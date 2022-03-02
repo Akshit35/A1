06b11948b079ec3c280b0ce3f1e2f2649c1971ac
Akshit35 <96778124+Akshit35@users.noreply.github.com>
2022-03-02 15:41:39 +0530

    Akshit Singhal(21104080)

---
+++ Assignment4 output.py
@@ -0,0 +1,115 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:/Users/hp/OneDrive/Desktop/Assignment 4.py ============
+
+         #question_1
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+
+          #question_2
+Enter number of rows in Pascal's Triangle: 15
+
+#using_recursion_method
+                            1   
+                          1   1   
+                        1   2   1   
+                      1   3   3   1   
+                    1   4   6   4   1   
+                  1   5   10   10   5   1   
+                1   6   15   20   15   6   1   
+              1   7   21   35   35   21   7   1   
+            1   8   28   56   70   56   28   8   1   
+          1   9   36   84   126   126   84   36   9   1   
+        1   10   45   120   210   252   210   120   45   10   1   
+      1   11   55   165   330   462   462   330   165   55   11   1   
+    1   12   66   220   495   792   924   792   495   220   66   12   1   
+  1   13   78   286   715   1287   1716   1716   1287   715   286   78   13   1   
+1   14   91   364   1001   2002   3003   3432   3003   2002   1001   364   91   14   1   
+
+#iterative_method
+                            1   
+                          1   1   
+                        1   2   1   
+                      1   3   3   1   
+                    1   4   6   4   1   
+                  1   5   10   10   5   1   
+                1   6   15   20   15   6   1   
+              1   7   21   35   35   21   7   1   
+            1   8   28   56   70   56   28   8   1   
+          1   9   36   84   126   126   84   36   9   1   
+        1   10   45   120   210   252   210   120   45   10   1   
+      1   11   55   165   330   462   462   330   165   55   11   1   
+    1   12   66   220   495   792   924   792   495   220   66   12   1   
+  1   13   78   286   715   1287   1716   1716   1287   715   286   78   13   1   
+1   14   91   364   1001   2002   3003   3432   3003   2002   1001   364   91   14   1   
+
+
+           #question_3
+
+Enter Integer:216
+Enter other Integer:25
+
+The quotient when 216 is divided by 25 is 8.
+The remainder when 216 is divided by 25 is 16.
+
+(a)
+
+Function is Not-callable
+
+(b)
+
+All result values are 'non-zero'
+
+(c)
+
+Append (4,5,6) in the values (8,16)
+Values greater than 4 is 8,16,5,6
+
+(d)
+
+Above result in set form is shown below:
+{8, 16, 5, 6}
+
+(e)
+
+The above set converted to immutable.
+
+(f)
+
+Max value from set is 16
+Hash value of 16 is 16
+
+         #question_4
+Hello, My name is Akshit and my roll no. is 21104080
+Destructor Called
+
+          #question_5
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+
+(a)
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+
+(b)
+Employee Viren data removed.
+
+           #question_6
+
+ FRIENDSHIP GAME
+
+Enter Player1 name:George
+Enter Player2 name:Barbie
+
+Enter Word by George:Laptop
+Enter Word by Barbie:Hiphop
+
+Friendship of George and Barbie is FAKE
